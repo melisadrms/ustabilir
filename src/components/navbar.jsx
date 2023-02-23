df27e5503867facac956107a4d24e597d6910d66
Melisa <74821468+melisadrms@users.noreply.github.com>
2023-02-23 17:09:11 +0300

    responsive navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react";
 import logoBigger from "../assets/logoBigger.png";
+import logoSmaller from "../assets/logoSmaller.png";
 import { useState } from "react";
 import DropDown from "./dropDown";
 
@@ -12,19 +13,31 @@ function Navbar() {
   console.log(isDropdownOpen);
 
   return (
-    <div className=" h-20 border shadow-lg shadow-slate-500/20 place-content-center grid ">
-      <div className="flex">
-        <img src={logoBigger} alt="logo" className=" h-11 w-32 mr-20 ml-3" />
-        <div className="flex gap-5 ">
-          <div className="flex" onClick={handleDropDown}>
-            <button>Kategoriler</button>
-            <Icon icon="gridicons:dropdown" className="self-center" />
-            {isDropdownOpen && <DropDown />}
+    <div className=" h-20 border shadow-lg shadow-slate-500/20 ">
+      <div className="flex md:justify-evenly mt-3">
+        <div className="flex">
+          <Icon
+            icon="icon-park:hamburger-button"
+            height="1.5rem"
+            className="mt-3 block md:hidden mr-5  "
+          />
+          <img
+            src={logoBigger}
+            alt="logo"
+            className="hidden md:block h-11 w-32 mr-10 "
+          />
+          <img src={logoSmaller} alt="logoSmall" className="block md:hidden" />
+          <div className=" hidden md:flex gap-5">
+            <div className="flex" onClick={handleDropDown}>
+              <button>Kategoriler</button>
+              <Icon icon="gridicons:dropdown" className="self-center" />
+              {isDropdownOpen && <DropDown />}
+            </div>
+            <button>Nasıl Çalışır?</button>
+            <button>Usta Rehberi</button>
           </div>
-          <button>Nasıl Çalışır?</button>
-          <button className="mr-96 ">Usta Rehberi</button>
         </div>
-        <button className="h-12 ml-40 w-40 rounded bg-[#f68761]">
+        <button className="h-12 w-40 rounded bg-[#f68761] max-md:ml-auto">
           Talep Oluştur
         </button>
       </div>
